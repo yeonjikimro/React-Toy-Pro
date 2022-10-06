e97387b37e10a8e7317202beafbee7a7a1f6f190
yeonjikimro <duswlskfk42@naver.com>
2022-10-07 02:17:22 +0900

    structure

---
+++ react-ss-project/src/App.js
@@ -0,0 +1,14 @@
+import Button from "../components/Button";
+import styles from "./App.module.css";
+
+function App() {
+  return (
+    <div>
+      <h1 className={styles.title}>Schedul Sharing Project</h1>
+      <Button text={"Continue"} />
+    </div>
+  );
+}
+
+export default App;
+ 
